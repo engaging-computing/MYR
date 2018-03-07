c8b20257145c873e6462f77e16143f39dc1f7b00
Chike Abuah <abuahchu@gmail.com>
2018-03-07 13:37:23 -0500

    extend myr with setters

--- src/myr/Myr.js
+++ src/myr/Myr.js
@@ -18,7 +18,36 @@ class Myr {
       y: 0,
       z: 0
     };
+    this.radius = 1;
   }
+
+  moveTo = (x, y, z) => {
+    return this.position = {
+      x: x,
+      y: y,
+      z: z
+    };
+  };
+  
+  setScale = (x, y, z) => {
+    return this.scale = {
+      x: x,
+      y: y,
+      z: z
+    };
+  };
+  
+  setRotation = (x, y, z) => {
+    return this.rotation = {
+      x: x,
+      y: y,
+      z: z
+    };
+  };
+
+  setRadius = (i) => {
+    return this.radius = i;
+  };
 
   getRandomColor = () => {
     var color, i, letters;
@@ -32,7 +61,7 @@ class Myr {
     return color;
   }
 
-  box = (x,y,z) => {
+  box = () => {
     let boxEl = document.createElement('a-box')
     boxEl.setAttribute('material', {
       color: this.getRandomColor()
@@ -43,6 +72,21 @@ class Myr {
     this.sceneEl.appendChild(boxEl);
     return boxEl;
   }
+
+  sphere = () => {
+    let sphereEl = document.createElement('a-sphere')
+    sphereEl.setAttribute('material', {
+      color: this.getRandomColor()
+    });
+    sphereEl.setAttribute('position', this.position);
+    sphereEl.setAttribute('scale', this.scale);
+    sphereEl.setAttribute('rotation', this.rotation);
+    sphereEl.setAttribute('radius', this.radius);    
+    this.sceneEl.appendChild(sphereEl);
+    return sphereEl;
+  }
+
+
 
 
 
