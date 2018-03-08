f2ad4292898874edf94f460debf72e53a523bb72
Chike Abuah <abuahchu@gmail.com>
2018-03-08 18:20:42 -0500

    implement drawer close

--- src/components/Reference.js
+++ src/components/Reference.js
@@ -8,10 +8,22 @@ export default class Reference extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {open: false};
+    this.state = {
+      open: false,
+      value: 'a',
+    };
   }
 
   handleToggle = () => this.setState({open: !this.state.open});
+
+  handleChange = (value) => {
+    if (value == 'x') {
+      value = this.state.value;
+    }
+    this.setState({
+      value: value,
+    });
+  };
 
   render() {
     const styles = {
@@ -32,10 +44,13 @@ export default class Reference extends React.Component {
           onClick={this.handleToggle}
         />
         <Drawer open={this.state.open}>
-          <Tabs>
+          <Tabs
+            value={this.state.value}
+            onChange={this.handleChange}>
             <Tab 
               icon={<FontIcon className="material-icons">change_history</FontIcon>}
-              label="PRIMITIVES">
+              label="PRIMITIVES"
+              value='a'>
               <div>
                 <h2 style={styles.headline}>Tab One</h2>
                 <p>
@@ -48,7 +63,8 @@ export default class Reference extends React.Component {
             </Tab>
             <Tab 
               icon={<FontIcon className="material-icons">build</FontIcon>}
-              label="HELPERS">
+              label="HELPERS"
+              value='b'>
               <div>
                 <h2 style={styles.headline}>Tab Two</h2>
                 <p>
@@ -59,7 +75,8 @@ export default class Reference extends React.Component {
             <Tab 
               icon={<FontIcon className="material-icons">close</FontIcon>}
               label="CLOSE"
-              onClick={this.handleToggle}>
+              value='x'
+              onActive={this.handleToggle}>
               <div>
                 <h2 style={styles.headline}>Tab Three</h2>
                 <p>
