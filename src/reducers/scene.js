7b758bf0453f07a4a609488418e7d6cad9d3cfd7
Chris Berns <cberns223@gmail.com>
2018-05-30 11:50:25 -0400

    const scene actions

--- src/reducers/scene.js
+++ src/reducers/scene.js
@@ -1,3 +1,5 @@
+import {LOAD_SCENE, NAME_SCENE} from '../actions/sceneActions';
+
 const initial_state = {
   name: "",
   id: "0",
@@ -9,12 +11,12 @@ const initial_state = {
 
 export default function scene(state = initial_state, action) {
   switch (action.type) {
-    case 'NAME_SCENE':
+    case NAME_SCENE:
       return {
         ...state,
         name: action.name
       };
-    case 'LOAD_SCENE':
+    case LOAD_SCENE:
       return {
         ...state,
         id: action.id
