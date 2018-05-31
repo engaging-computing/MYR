6dc6ce979a4fde8dcae4a8b41d64dd26bf06321a
Chris Berns <cberns223@gmail.com>
2018-05-31 10:52:35 -0400

    auth now uses const values

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,15 +1,17 @@
+import {LOGIN, LOGOUT} from '../actions/authActions';
+
 const initial_state = {
   user: null,
 };
 
 export default function user(state = initial_state, action) {
   switch (action.type) {
-    case 'LOGIN':
+    case LOGIN:
       return {
         ...state,
         user: action.user
       };
-    case 'LOGOUT':
+    case LOGOUT:
       return {
         ...state,
         user: null
