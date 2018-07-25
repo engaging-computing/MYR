579ace2c4497f8c1376f9851b66b01fa3af757ea
Elena Izotova <elena.k.izotova@gmail.com>
2018-07-24 20:24:45 -0400

    Updated and coloured reference section

--- src/myr/reference.js
+++ src/myr/reference.js
@@ -2,20 +2,14 @@ import React from 'react';
 var geometry = [
   {
     name: 'box()',
-    parameters: ["(Object) attributes = {\n material: {color: 'red'},\n position: '0 0 -2',\n scale: '1 2 3',\n rotation: '0 45 90' };"],
-    returnValue: 'js object representation of the Box primitive',
     description: "Renders a box using current internal MYR properties"
   },
   {
     name: 'circle()',
-    parameters: ["(Object) attributes = {\n radius: 1,\n material: {color: 'red'},\n position: '0 0 -2',\n scale: '1 2 3',\n rotation: '0 45 90' };"],
-    returnValue: 'js object representation of the Circle primitive',
     description: "Renders a circle using current internal MYR properties"
   },
   {
     name: 'cone()',
-    parameters: ["(Object) attributes = {\n bottomRadius: 4,\n topRadius: 1,\n material: {color: 'red'},\n position: '0 0 -2',\n scale: '1 2 3',\n rotation: '0 45 90' };"],
-    returnValue: 'js object representation of the Cone primitive',
     description: "Renders a cone using current internal MYR properties"
   },
   {
@@ -24,26 +18,18 @@ var geometry = [
   },
   {
     name: 'dodecahedron()',
-    parameters: ["(Object) attributes = {\n radius: 1,\n material: {color: 'red'},\n position: '0 0 -2',\n scale: '1 2 3',\n rotation: '0 45 90' };"],
-    returnValue: 'js object representation of the Dodecahedron primitive',
     description: "Renders a dodecahedron using current internal MYR properties"
   },
   {
     name: 'icosahedron()',
-    parameters: ["(Object) attributes =  {\n radius: 1,\n material: {color: 'red'},\n position: '0 0 -2',\n scale: '1 2 3',\n rotation: '0 45 90' };"],
-    returnValue: 'js object representation of the Icosahedron primitive',
     description: "Renders a icosahedron using current internal MYR properties"
   },
   {
     name: 'octahedron()',
-    parameters: ["(Object) attributes =  {\n radius: 1,\n material: {color: 'red'},\n position: '0 0 -2',\n scale: '1 2 3',\n rotation: '0 45 90' };"],
-    returnValue: 'js object representation of the Octahedron primitive',
     description: "Renders a octahedron using current internal MYR properties"
   },
   {
     name: 'prism()',
-    parameters: ["(Object) attributes =  {\n segments: 5,\n material: {color: 'red'},\n position: '0 0 -2',\n scale: '1 2 3',\n rotation: '0 45 90' };"],
-    returnValue: 'js object representation of the Prism primitive',
     description: "Renders a prism using current internal MYR properties. Pass a obj.segments value greater than 2."
   },
   {
@@ -60,8 +46,6 @@ var geometry = [
   },
   {
     name: 'text()',
-    parameters: ["(Object) attributes =  {\n value: 'Hello World',\n material: {color: 'red'},\n position: '0 0 -2',\n scale: '1 2 3',\n rotation: '0 45 90' };"],
-    returnValue: 'js object representation of the Text primitive',
     description: "Renders text using current internal MYR properties"
   },
   {
@@ -74,126 +58,127 @@ var geometry = [
   },
   {
     name: 'triangle()',
-    parameters: ["(Object) attributes =  {\n material: {color: 'red'},\n position: '0 0 -2',\n scale: '1 2 3',\n rotation: '0 45 90' };"],
-    returnValue: 'js object representation of the Triangle primitive',
     description: "Renders a triangle using current internal MYR properties"
   },
 
 ];
 var transformations = [
   {
-    name: 'setColor("color")',
-    description: <a href='https://htmlcolorcodes.com/color-names/' target="_blank" rel="noopener noreferrer">using these color codes</a>
+    name: <span>setColor(<span className="string">color</span>)</span>,
+    description: <span>Changes the color <a href='https://htmlcolorcodes.com/color-names/' target="_blank" rel="noopener noreferrer">using these color codes</a>.</span>
   },
   {
-    name: 'setPosition(x,y,z)',
-    parameters: [
-      "(Number) x =  0;",
-      "(Number) y =  1;",
-      "(Number) z =  -2;",
-    ],
-    returnValue: 'js object representation of the new position',
+    name: <span>setPosition(<span className="number">x</span>,<span className="number">y</span>,<span className="number">z</span>)</span>,
     description: "Sets a new 3D position"
   },
   {
-    name: 'setXPos(x)',
-    description: "Sets a new x position"
+    name: <span>setXPos(<span className="number">x</span>)</span>,
+    description: <span>Sets a new <span className="number">x</span> position</span>
   },
   {
-    name: 'setYPos(y)',
-    description: "Sets a new y position"
+    name: <span>setYPos(<span className="number">y</span>)</span>,
+    description: <span>Sets a new <span className="number">y</span> position</span>
   },
   {
-    name: 'setZPos(z)',
-    description: "Sets a new z position"
+    name: <span>setZPos(<span className="number">z</span>)</span>,
+    description: <span>Sets a new <span className="number">z</span> position</span>
   },
   {
-    name: 'setScale(x,y,z)',
-    parameters: [
-      "(Number) x =  0;",
-      "(Number) y =  1;",
-      "(Number) z =  -2;",
-    ],
-    returnValue: 'js object representation of the new scale',
+    name: <span>setScale(<span className="number">x</span>,<span className="number">y</span>,<span className="number">z</span>)</span>,
     description: "Sets a new 3D scaling factor"
   },
   {
-    name: 'setXScale(x)',
-    description: "Sets a new x scaling factor"
+    name: <span>setXScale(<span className="number">x</span>)</span>,
+    description: <span>Sets a new <span className="number">x</span> scaling factor</span>
   },
   {
-    name: 'setYScale(y)',
-    description: "Sets a new y scaling factor"
+    name: <span>setYScale(<span className="number">y</span>)</span>,
+    description: <span>Sets a new <span className="number">y</span> scaling factor</span>
   },
   {
-    name: 'setZScale(z)',
-    description: "Sets a new z scaling factor"
+    name: <span>setZScale(<span className="number">z</span>)</span>,
+    description: <span>Sets a new <span className="number">z</span> scaling factor</span>
   },
   {
-    name: 'setRotation(x,y,z)',
-    parameters: [
-      "(Number) x =  0;",
-      "(Number) y =  1;",
-      "(Number) z =  -2;",
-    ],
-    returnValue: 'js object representation of the new rotation',
+    name: <span>setRotation(<span className="number">x</span>, <span className="number">y</span>, <span className="number">z</span>)</span>,
     description: "Sets new 3D rotation angles"
   },
   {
-    name: 'pitchX(x)',
-    description: "Sets a new x rotation angle"
+    name: <span>pitchX(<span className="number">x</span>)</span>,
+    description: <span>Sets a new <span className="number">x</span> rotation angle</span>
   },
   {
-    name: 'yawY(y)',
-    description: "Sets a new y rotation angle"
+    name: <span>yawY(<span className="number">y</span>)</span>,
+    description: <span>Sets a new <span className="number">y</span> rotation angle</span>
   },
   {
-    name: 'rollZ(z)',
-    description: "Sets a new z rotation angle"
+    name: <span>rollZ(<span className="number">z</span>)</span>,
+    description: <span>Sets a new <span className="number">z</span> rotation angle</span>
   },
   {
-    name: 'setRadius(r)',
-    parameters: [
-      "(Number) r =  1;",
-    ],
-    returnValue: 'js object representation of the new radius',
-    description: "Sets a new radius attribute"
+    name: <span>setRadius(<span className="number">radius</span>)</span>,
+    description: <span>Sets a new <span className="number">radius</span> attribute</span>
   },
 ];
-
-var webvr_components = [
+var animations = [
   {
-    name: 'setCursor(x,y,z)',
-    parameters: [
-      "(Number) x =  0;",
-      "(Number) y =  1;",
-      "(Number) z =  -2;",
-    ],
-    returnValue: 'js object representation of the new cursor',
-    description: `Sets a cursor at the specified 3D position.`
+    name: <span>spin(<span className="string">element</span>, <span className="number">magnitude</span>, <span className="bool">loop</span>, <span className="number">duration</span>)</span>,
+    description: <span>Spins the element on the y axis <span className="number">magnitude</span> degrees.</span>
   },
   {
-    name: 'setCamera(x,y,z)',
-    parameters: [
-      "(Number) x =  0;",
-      "(Number) y =  1;",
-      "(Number) z =  -2;",
-    ],
-    returnValue: 'js object representation of the new camera',
-    description: "Sets the camera to a new 3D position"
+    name: <span>yoyo(<span className="string">element</span>, <span className="number">magnitude</span>, <span className="bool">loop</span>, <span className="number">duration</span>)</span>,
+    description: <span>Bounces the element <span className="number">magnitude</span> units in a positive direction on the y axis.</span>
   },
   {
-    name: 'setLight()',
-    parameters: ["(Object) attributes =\n { type: 'directional' || 'point' || 'ambient',\n color: 'red',\n position: '0 0 -2'};"],
-    returnValue: 'js object representation of the Light primitive',
-    description: "Renders a light using current internal MYR properties"
+    name: <span>sideToSide(<span className="string">element</span>, <span className="number">magnitude</span>, <span className="bool">loop</span>, <span className="number">duration</span>)</span>,
+    description: <span>Shifts the element <span className="number">magnitude</span> units in a negative direction on the x axis and back to the original coordinate.</span>
+  },
+  {
+    name: <span>goUp(<span className="string">element</span>, <span className="number">magnitude</span>, <span className="bool">loop</span>, <span className="number">duration</span>)</span>,
+    description: <span>Translates the element <span className="number">magnitude</span> units in a positive direction on the y axis.</span>
+  },
+  {
+    name: <span>goDown(<span className="string">element</span>, <span className="number">magnitude</span>, <span className="bool">loop</span>, <span className="number">duration</span>)</span>,
+    description: <span>Translates the element <span className="number">magnitude</span> units in a negative direction on the y axis.</span>
+  },
+  {
+    name: <span>goRight(<span className="string">element</span>, <span className="number">magnitude</span>, <span className="bool">loop</span>, <span className="number">duration</span>)</span>,
+    description: <span>Translates the element <span className="number">magnitude</span> units in a positive direction on the x axis.</span>
+  },
+  {
+    name: <span>goLeft(<span className="string">element</span>, <span className="number">magnitude</span>, <span className="bool">loop</span>, <span className="number">duration</span>)</span>,
+    description: <span>Translates the element <span className="number">magnitude</span> units in a negative direction on the x axis.</span>
+  },
+  {
+    name: <span>goTowards(<span className="string">element</span>, <span className="number">magnitude</span>, <span className="bool">loop</span>, <span className="number">duration</span>)</span>,
+    description: <span>Translates the element <span className="number">magnitude</span> units in a positive direction on the z axis.</span>
+  },
+  {
+    name: <span>goAway(<span className="string">element</span>, <span className="number">magnitude</span>, <span className="bool">loop</span>, <span className="number">duration</span>)</span>,
+    description: <span>Translates the element <span className="number">magnitude</span> units in a negative direction on the z axis.</span>
+  },
+  {
+    name: <span>grow(<span className="string">element</span>, <span className="number">magnitude</span>, <span className="bool">loop</span>, <span className="number">duration</span>)</span>,
+    description: <span>Scales the element by a <span className="number">magnitude</span> multiplier.</span>
+  },
+  {
+    name: <span>shrink(<span className="string">element</span>, <span className="number">magnitude</span>, <span className="bool">loop</span>, <span className="number">duration</span>)</span>,
+    description: <span>Scales the element by a 1/<span className="number">magnitude</span> multiplier.</span>
+  },
+  {
+    name: <span>fadeOut(<span className="string">element</span>, <span className="number">magnitude</span>, <span className="bool">loop</span>, <span className="number">duration</span>)</span>,
+    description: <span>Modifies transparency from 1 to <span className="number">magnitude</span> [0,1).</span>
+  },
+  {
+    name: <span>fadeIn(<span className="string">element</span>, <span className="number">magnitude</span>, <span className="bool">loop</span>, <span className="number">duration</span>)</span>,
+    description: <span>Modifies transparency from 0 to <span className="number">magnitude</span> (0,1].</span>
   },
 ];
 
 const reference = {
   geometry: geometry,
   transformations: transformations,
-  webvr_components: webvr_components,
+  animations: animations,
 };
 
 export default function r(ref = reference) {
