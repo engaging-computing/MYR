790a02acb3f15441b9360e3e031df57c74d4ad40
Chris Berns <cberns223@gmail.com>
2018-11-30 19:42:02 -0500

    Disabling arrow keys so they do not scroll the page

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -273,6 +273,12 @@ var customCompleter = {
 class Editor extends Component {
 
   componentDidMount() {
+    window.addEventListener("keydown", function (e) {
+      // space and arrow keys
+      if ([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+      }
+    }, false);
     this.refs.aceEditor.editor.completers = [customCompleter];
   }
 
