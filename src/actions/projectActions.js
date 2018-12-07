5b62736e1127a77cdacf5fa7aa265432d09f4d03
Jason <jason_kiesling@student.uml.edu>
2018-12-07 13:23:47 -0500

    Redirect when deleting

--- src/actions/projectActions.js
+++ src/actions/projectActions.js
@@ -67,7 +67,7 @@ export function deleteProj(id, name) {
     let imgRef = storageRef.child(path);
 
     // If deleting current project, redirect to home
-    if (window.location.href === window.origin + '/' + id) {
+    if (window.location.href === window.origin + '/' + id || window.location.href === window.origin + '/' + id + '/') {
       window.location.href = window.origin;
     }
 
@@ -83,7 +83,7 @@ export function deleteProj(id, name) {
     }).catch((error) => {
       console.error("Error removing document: ", error);
     });
-    
+
     return { type: DELTE_PROJ, id: id };
   }
 }
