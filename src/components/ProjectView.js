56a791f00ed83eeb8575da4b4a4f04f5d78f122c
Chris Berns <cberns223@gmail.com>
2019-01-10 10:14:57 -0500

    Formatting

--- src/components/ProjectView.js
+++ src/components/ProjectView.js
@@ -1,95 +1,95 @@
 import React from 'react';
 import {
-  Button,
-  Icon
+	Button,
+	Icon
 } from '@material-ui/core';
 
 class ProjectView extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      deleteFunc: this.props.deleteFunc,
-      showImg: false
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			deleteFunc: this.props.deleteFunc,
+			showImg: false
+		};
+	}
 
-  helper = (proj, canDelete) => {
-    if (proj) {
-      let id = proj.id;
-      let name = proj.data.name;
-      return (
-        <div key={id} id={id} title={name}
-          className="col-xs-12 col-md-6 col-lg-4 pt-2 pl-0">
-          <a href={`/${id}`} >
-            <span className="project-span">{name}</span>
-            <img id={id} alt={id} src={proj.url}
-              className={"img-thumbnail " + (this.state.showImg && "d-none")} />
-          </a>
-          {canDelete ?
-            <Button
-              label="delete Project"
-              color="secondary"
-              className="delete-btn"
-              fullWidth={!this.state.showImg}
-              onClick={() => this.props.deleteFunc(id, proj.data.name)}>
-              <Icon className="material-icons">delete</Icon>
-            </Button>
-            : null
-          }
-        </div>
-      );
-    } else {
-      return null;
-    }
-  }
+	helper = (proj, canDelete) => {
+		if (proj) {
+			let id = proj.id;
+			let name = proj.data.name;
+			return (
+				<div key={id} id={id} title={name}
+					className="col-xs-12 col-md-6 col-lg-4 pt-2 pl-0">
+					<a href={`/${id}`} >
+						<span className="project-span">{name}</span>
+						<img id={id} alt={id} src={proj.url}
+							className={"img-thumbnail " + (this.state.showImg && "d-none")} />
+					</a>
+					{canDelete ?
+						<Button
+							label="delete Project"
+							color="secondary"
+							className="delete-btn"
+							fullWidth={!this.state.showImg}
+							onClick={() => this.props.deleteFunc(id, proj.data.name)}>
+							<Icon className="material-icons">delete</Icon>
+						</Button>
+						: null
+					}
+				</div>
+			);
+		} else {
+			return null;
+		}
+	}
 
-  render() {
-    let previewToggle = {
-      position: 'fixed',
-      top: 5
-    };
-    const userProjs = [].concat(this.props.userProjs);
-    const examplProjs = [].concat(this.props.examplProjs);
-    return (
-      <div id="project-list" >
-        <Button
-          style={previewToggle}
-          onClick={() => this.setState({ showImg: !this.state.showImg })}>
-          { // If we are showing the img, show the proper icon
-            this.state.showImg
-              ?
-              <Icon className="material-icons">visibility_off</Icon>
-              :
-              <Icon className="material-icons">visibility</Icon>
-          }
-          <div>&nbsp;</div>Preview
+	render() {
+		let previewToggle = {
+			position: 'fixed',
+			top: 5
+		};
+		const userProjs = [].concat(this.props.userProjs);
+		const examplProjs = [].concat(this.props.examplProjs);
+		return (
+			<div id="project-list" >
+				<Button
+					style={previewToggle}
+					onClick={() => this.setState({ showImg: !this.state.showImg })}>
+					{ // If we are showing the img, show the proper icon
+						this.state.showImg
+							?
+							<Icon className="material-icons">visibility_off</Icon>
+							:
+							<Icon className="material-icons">visibility</Icon>
+					}
+					<div>&nbsp;</div>Preview
         </Button>
-        <div className="row" id="user-proj" style={{ width: "100%" }}>
-          <h3 className="col-12 p-0 mb-3 border-bottom"> Your Projects
+				<div className="row" id="user-proj" style={{ width: "100%" }}>
+					<h3 className="col-12 p-0 mb-3 border-bottom"> Your Projects
           </h3>
-          <hr />
-          { // Sort the users projects in alphabetical order
-            userProjs.sort(function (a, b) {
-              return a.name < b.name ? -1 : a.name > b.name ? 1 : 0;
-            }).map(proj => {
-              return this.helper(proj, true);
-            })
-          }
-        </div>
-        <div className="row" id="sample-proj" style={{ width: "100%" }}>
-          <h3 className="col-12 p-2 mb-3 border-bottom">Sample Projects</h3>
-          <hr />
-          { // Sort the examples projects in alphabetical order
-            examplProjs.sort(function (a, b) {
-              return a.name < b.name ? -1 : a.name > b.name ? 1 : 0;
-            }).map(proj => {
-              return this.helper(proj, false);
-            })
-          }
-        </div>
-      </div>
-    );
-  }
+					<hr />
+					{ // Sort the users projects in alphabetical order
+						userProjs.sort(function (a, b) {
+							return a.name < b.name ? -1 : a.name > b.name ? 1 : 0;
+						}).map(proj => {
+							return this.helper(proj, true);
+						})
+					}
+				</div>
+				<div className="row" id="sample-proj" style={{ width: "100%" }}>
+					<h3 className="col-12 p-2 mb-3 border-bottom">Sample Projects</h3>
+					<hr />
+					{ // Sort the examples projects in alphabetical order
+						examplProjs.sort(function (a, b) {
+							return a.name < b.name ? -1 : a.name > b.name ? 1 : 0;
+						}).map(proj => {
+							return this.helper(proj, false);
+						})
+					}
+				</div>
+			</div>
+		);
+	}
 }
 
 export default ProjectView;
--- src/reducers/course.js
+++ src/reducers/course.js
@@ -1,53 +1,53 @@
 import { SYNC_COURSES, LOAD_COURSE, LOAD_LESSON, SET_INDEX } from '../actions/courseActions';
 
 const initial_state = {
-    courses: [],
-    course: {},
-    currentIndex: 0,
-    currentLesson: {
-        name: "Loading....",
-        id: -1,
-        prompt: "Please wait",
-        code: "// Loading"
-    }
+  courses: [],
+  course: {},
+  currentIndex: 0,
+  currentLesson: {
+    name: "Loading....",
+    id: -1,
+    prompt: "Please wait",
+    code: "// Loading"
+  }
 };
 
 export default function course(state = initial_state, action) {
-    let payload;
-    switch (action.type) {
-        case SYNC_COURSES:
-            return {
-                ...state,
-                courses: action.payload
-            };
-        case LOAD_COURSE:
-            return {
-                ...state,
-                course: action.payload
-            };
-        case LOAD_LESSON:
-            payload = action.payload;
-            return {
-                ...state,
-                currentLesson: {
-                    name: payload.name,
-                    id: payload._id,
-                    prompt: payload.prompt,
-                    code: payload.code
-                }
-            };
-        case SET_INDEX:
-            return {
-                ...state,
-                currentIndex: action.payload,
-                currentLesson: {
-                    name: state.name,
-                    id: state._id,
-                    prompt: state.prompt,
-                    code: state.code
-                }
-            };
-        default:
-            return state;
-    }
+  let payload;
+  switch (action.type) {
+    case SYNC_COURSES:
+      return {
+        ...state,
+        courses: action.payload
+      };
+    case LOAD_COURSE:
+      return {
+        ...state,
+        course: action.payload
+      };
+    case LOAD_LESSON:
+      payload = action.payload;
+      return {
+        ...state,
+        currentLesson: {
+          name: payload.name,
+          id: payload._id,
+          prompt: payload.prompt,
+          code: payload.code
+        }
+      };
+    case SET_INDEX:
+      return {
+        ...state,
+        currentIndex: action.payload,
+        currentLesson: {
+          name: state.name,
+          id: state._id,
+          prompt: state.prompt,
+          code: state.code
+        }
+      };
+    default:
+      return state;
+  }
 }
