9dd8c741ea7ef09ca1133e7c6b8a616a9228b784
Jason <jason_kiesling@student.uml.edu>
2019-06-06 20:13:26 -0400

    Fixed lint issue

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -20,7 +20,7 @@ const reducer = combineReducers({
   referenceExample
 });
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ trace: true, traceLimit: 25 }) || compose;
+const composeEnhancers = (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ trace: true, traceLimit: 25 })) || compose;
 
 const store = createStore(
   reducer,
