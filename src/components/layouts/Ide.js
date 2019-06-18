a48cd5dc6e88f48ff1c67f717448e03d566276b3
jSwanson99 <john@swansonwriting.com>
2019-06-18 09:49:05 -0400

    Fixed bug where view only toggle doesn't work in courses (#238)
    * Render button should behave properly/the same as crtl enter
    
    * Fixed bug where view only toggle doesn't work in courses
    
    * removed unnecessary lines of code

--- src/components/layouts/Ide.js
+++ src/components/layouts/Ide.js
@@ -26,8 +26,6 @@ export const Ide = ({ editor, editorActions, user, authActions, scene, sceneActi
             collectionActions={collectionActions}
             collections={collections}
             layoutType={layoutTypes.IDE}
-            obj={editor.objects}
-            editor={editor}
         />
         <div className="row no-gutters">
             {
