141443308af3d76a629f47eacc568e79d5233e4a
Jason Kiesling <jason@learnmyr.org>
2019-06-18 20:29:55 -0400

    Firebase import updates

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,7 @@
-import firebase from "firebase";
+import firebase from "@firebase/app";
+import "@firebase/auth";
+import "@firebase/database";
+import "@firebase/storage";
 import "@firebase/firestore";
 import firebaseKey from "./keys/firebase.js";
 
