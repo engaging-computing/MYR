27e88f5f6ec1c01409cef78bd2e122cb01bb2353
Jason Kiesling <jason@learnmyr.org>
2019-06-23 15:57:25 -0400

    Implemented welcome screen (#265)
    * Course select state in header
    
    * Tour state in header
    
    * exampl -> example
    
    * Formatting and ability to hide icon
    
    * Implement initial welcome screen
    
    * Remove welcome text
    
    * Linting for switch cases

---
+++ src/reducers/classes.js
@@ -0,0 +1,32 @@
+import * as types from "../constants/ActionTypes";
+
+const initial_state = {
+    classrooms: [],
+    classroom: []
+};
+
+export default function classes(state = initial_state, action) {
+    switch (action.type) {
+        case types.SYNC_CLASSES:
+            return {
+                ...state,
+                classrooms: action.payload
+            };
+        case types.SYNC_CLASS:
+            return {
+                ...state,
+                classroom: action.payload
+            };
+        case types.DELETE_CLASS:
+            let userClasses = [];
+            userClasses = state.classrooms.filter(x => {
+                return x.id !== action.id;
+            });
+            return {
+                ...state,
+                classrooms: userClasses
+            };
+        default:
+            return state;
+    }
+}
