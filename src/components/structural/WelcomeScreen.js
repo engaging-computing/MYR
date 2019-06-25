47a8557c752b211c1f23f058c3f54a981c5a3ae4
Jason Kiesling <jason@learnmyr.org>
2019-06-24 21:02:08 -0400

    Welcome screen is GDPR compliant

--- src/components/structural/WelcomeScreen.js
+++ src/components/structural/WelcomeScreen.js
@@ -70,16 +70,10 @@ class Welcome extends React.Component {
             projectsTab: "b",
             coursesOpen: false
         };
-        this.emailRef = React.createRef();
     }
 
     componentDidMount() {
-        if (!this.getCookie("firstTime")) {
-            let date = new Date();
-            date.setTime(date.getTime() + (1000 * 60 * 60 * 24));
-            let expiration = "; expires=" + date.toGMTString();
-            let cookie = "firstTime=true" + expiration;
-            document.cookie = cookie;
+        if (!this.getCookie("hasVisited")) {
             this.setState({ welcomeOpen: true });
         }
     }
@@ -100,12 +94,23 @@ class Welcome extends React.Component {
         return "";
     }
 
+    setCookie = () => {
+        if (!this.getCookie("hasVisited")) {
+            let date = new Date();
+            date.setTime(date.getTime() + (1000 * 60 * 60 * 24));
+            let expiration = "; expires=" + date.toGMTString();
+            let cookie = "hasVisited=true" + expiration;
+            document.cookie = cookie;
+        }
+    }
+
     handleClose = () => {
+        this.setCookie();
         this.setState({ welcomeOpen: false });
     };
 
     neverAgainCookie = () => {
-        document.cookie = "firstTime=true; expires=Thu, 31 Dec 2099 12:00:00 UTC;";
+        document.cookie = "hasVisited=true; expires=Thu, 31 Dec 2099 12:00:00 UTC;";
         this.handleClose();
     }
 
@@ -114,21 +119,24 @@ class Welcome extends React.Component {
             <Button
                 onClick={this.neverAgainCookie}
                 className="neverAgain-btn">
-                Never show this screen again
+                Don't show again
             </Button >
         );
     }
 
     handleProjectToggle = () => {
+        this.setCookie();
         this.setState({ projectsOpen: !this.state.projectsOpen });
         this.setState({ projectTab: "b" });
     };
 
     handleCoursesToggle = () => {
+        this.setCookie();
         this.setState({ coursesOpen: !this.state.coursesOpen });
     };
 
     handleTourToggle = () => {
+        this.setCookie();
         this.setState({ tourOpen: !this.state.tourOpen });
     };
 
@@ -159,6 +167,7 @@ class Welcome extends React.Component {
                         href="/reference"
                         target="_blank"
                         rel="noopener noreferrer"
+                        onClick={this.setCookie}
                         className="welcome-btn">
                         <Icon className="material-icons">help</Icon>
                         Open the Reference
@@ -182,6 +191,7 @@ class Welcome extends React.Component {
                         href="/about/support"
                         target="_blank"
                         rel="noopener noreferrer"
+                        onClick={this.setCookie}
                         className="welcome-btn">
                         <Icon className="material-icons">alternate_email</Icon>
                         Get Support
@@ -193,6 +203,7 @@ class Welcome extends React.Component {
                         href="https://github.com/engaging-computing/MYR"
                         target="_blank"
                         rel="noopener noreferrer"
+                        onClick={this.setCookie}
                         className="welcome-btn">
                         <Icon className="material-icons">code</Icon>
                         Visit our GitHub
@@ -258,6 +269,8 @@ class Welcome extends React.Component {
                             <this.handleModals />
                             <hr />
                             <this.neverAgain />
+                            <hr />
+                            <p id="cookie-consent" className="text-center">MYR uses cookies which are necessary for its functioning. You accept the use of cookies by continuing to use MYR per the <a href="/about/privacy" target="_blank" rel="noopener noreferrer">privacy policy</a>.</p>
                         </div>
                     </Modal>
                 </React.Fragment>
