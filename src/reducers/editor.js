3bc80a401e669a334b4303a6d661da95006c0d7b
jlu18 <jlu18@DESKTOP-H9FOA0Q.localdomain>
2019-07-11 13:17:11 -0400

    conflict

--- src/reducers/editor.js
+++ src/reducers/editor.js
@@ -3,24 +3,24 @@ import * as types from "../constants/ActionTypes";
 import Myr from "../myr/Myr";
 
 const initial_state = {
-  text: "",
-  objects: [],
-  assets: [],
-  message: {
     text: "",
-    time: 0
-  }
+    objects: [],
+    assets: [],
+    message: {
+        text: "",
+        time: 0
+    }
 };
 
 /**
 * @summary - Snapshots is an array of objects that record each time the user tries to render
 */
 let snapshots = [
-  {
-    timestamp: Date.now(),
-    text: `${initial_state.text}`,
-    error: false
-  }
+    {
+        timestamp: Date.now(),
+        text: `${initial_state.text}`,
+        error: false
+    }
 ];
 
 
@@ -30,21 +30,14 @@ m.init();
 
 // ESLint doesn't like this but it is better than eval
 function noEvalEvaluation(text) {
-<<<<<<< HEAD
     // eslint-disable-next-line
     // let func = Function(`'use strict'; ${m.infiniteLoopDetector.wrap(text)}`);
     // eslint-disable-next-line
     let func = Function(`'use strict'; ${text}`);
     return func;
-=======
-  // eslint-disable-next-line
-  return Function(`'use strict'; ${text}`)();
->>>>>>> Render button should behave properly/the same as crtl enter (#237)
 }
 
 export default function editor(state = initial_state, action) {
-
-<<<<<<< HEAD
     switch (action.type) {
         case types.EDITOR_RENDER:
             m.reset();
@@ -109,69 +102,4 @@ export default function editor(state = initial_state, action) {
         default:
             return state;
     }
-=======
-  switch (action.type) {
-    case types.EDITOR_RENDER:
-      m.reset();
-
-      // build an object to save the snap
-      let snap = {
-        user: action.uid ? action.uid : "unknown",
-        timestamp: Date.now(),
-        text: action.text,
-        error: false
-      };
-
-      let message = {
-        text: "",
-        time: Date.now()
-      };
-
-      try {
-        noEvalEvaluation(action.text);
-      }
-      catch (err) {
-        // Notify that eval failed
-        console.error("Eval failed: " + err);
-        message = { ...message, text: "Eval failed: " + err };
-        snap = { ...snap, error: true };
-      }
-
-      snapshots.push(snap);
-
-      fetch("/apiv1/snapshots/", {
-        headers: new Headers({ "Content-Type": "application/json" }),
-        method: "post",
-        body: JSON.stringify(snap)
-      });
-
-      return {
-        ...state,
-        text: action.text,
-        objects: m.els || [],
-        assets: m.assets || [],
-        message
-      };
-
-    case types.EDITOR_REFRESH:
-      m.reset();
-      return {
-        ...initial_state,
-        text: action.text
-      };
-
-    case types.EDITOR_RECOVER:
-      // Start at last snap
-      let stableIndex = snapshots.length - 1;
-      // Work backwards until we find a non-error snap
-      while (snapshots[stableIndex].error === true) {
-        stableIndex--;
-      }
-
-      // Call editor function again with new params
-      return editor({ ...state }, { type: types.EDITOR_RENDER, text: snapshots[stableIndex].text });
-    default:
-      return state;
-  }
->>>>>>> Render button should behave properly/the same as crtl enter (#237)
 }
