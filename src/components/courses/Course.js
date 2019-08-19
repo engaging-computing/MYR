d47a1429a0ab9ae64f46093aa9b4d9ddd7a4bf8c
jlu18 <Justin_Lu@student.uml.edu>
2019-08-19 13:03:52 -0400

    change the function name to hasEditorChange

--- src/components/courses/Course.js
+++ src/components/courses/Course.js
@@ -10,7 +10,7 @@ class Lesson extends Component {
     nextLesson = () => {
         const currentIndex = this.props.courses.currentIndex;
         const lessons = this.props.course.lessons;
-        if(this.isEditorChange()){
+        if(this.hasEditorChange()){
             if (window.confirm("Are you sure you want to continue?\nYou will lose any unsaved work!")) {
                 this.props.courseActions.nextLesson(currentIndex, lessons[currentIndex + 1]);
             }
@@ -22,7 +22,7 @@ class Lesson extends Component {
     lastLesson = () => {
         const currentIndex = this.props.courses.currentIndex;
         const lessons = this.props.course.lessons;
-        if(this.isEditorChange()){
+        if(this.hasEditorChange()){
             if (window.confirm("Are you sure you want to continue?\nYou will lose any unsaved work!")) {
                 this.props.courseActions.previousLesson(currentIndex, lessons[currentIndex - 1]);
             }
