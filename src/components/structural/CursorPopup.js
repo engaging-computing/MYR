fe80ce70e9d4db60114ea7a2791b14afd9a72f8c
John Swanson <john@swansonwriting.com>
2019-08-21 09:44:43 -0400

    added clickable icons to displays objects within cursor

--- src/components/structural/CursorPopup.js
+++ src/components/structural/CursorPopup.js
@@ -1,5 +1,9 @@
 import React, {Component} from 'react';
-import Popover from '@material-ui/core/Popover';
+import {
+        Icon, 
+        IconButton,
+        Popover,
+    } from '@material-ui/core/';
 
 
 class CursorPopup extends Component {
@@ -9,6 +13,10 @@ class CursorPopup extends Component {
         this.state = {   
             anchorEl: null,
             obj: {},
+            posOpen: false,
+            scaleOpen: false,
+            rotOpen: false,
+            magOpen: false
         }
     }
 
@@ -44,28 +52,114 @@ class CursorPopup extends Component {
 
     }
 
-    helper = (key, value) => {
+    handleButtonClick = key => {
+        console.log("clicked")
+        switch (key) {
+            case "position":
+                this.setState({
+                    posOpen: !this.state.posOpen
+                })
+                break;
+            case "scale":
+                this.setState({
+                    scaleOpen: !this.state.scaleOpen
+                })
+                break;
+            case "rotation":
+                this.setState({
+                    rotOpen: !this.state.rotOpen
+                })
+                break;
+            case "magnitude":
+                this.setState({
+                    magOpen: !this.state.magOpen
+                })
+                break;
+            default:
+                break;
+        }
+    }
+
+    getOpen = key => {
+        switch (key) {
+            case "position":
+                return this.state.posOpen ? "block" : "none"
+            case "scale":
+                return this.state.scaleOpen ? "block" : "none"
+            case "rotation":
+                    return this.state.rotOpen ? "block" : "none"
+            case "magnitude":
+                    return this.state.magOpen ? "block" : "none"
+            default:
+                break;
+        }
+    }
+
+    helper = (key, value, firstPass) => {
         const style = {
             "padding" : "0px",
-            "padding-left": "15px",
-            "margin": "0px"
+            "paddingLeft": "15px",
+            "margin": "0px",
+            "display": "inline-block"
         }
-        if(typeof value === 'object' && value !== null) {
+
+        if(typeof value === 'object' && value !== null && !firstPass) {
             let str = Object.keys(value).map(k => {
-                return this.helper(k, value[k]);
+                return this.helper(k, value[k], (Boolean(typeof value[k] !== "object")));
             })
+
+  
+            let iconType = "";
+
+            switch (key) {
+                case "position":
+                    iconType = "my_location";
+                    break;
+                case "scale":
+                    iconType = "zoom_out_map";
+                    break;
+                case "rotation":
+                    iconType = "sync";
+                    break;
+                case "magnitude":
+                    iconType = "transform";
+                    break;
+                default:
+                    iconType = "help";
+                    break;
+            }
+
+            const shouldDisplayStyle = {
+                "display": this.getOpen(key)
+            }
+
+            console.log(shouldDisplayStyle)
+
             return (
-                <div style={style}>
-                    <h6>{key}</h6>
-                    <p>{str}</p>
+                <div id = "objectContainer" style={style}>
+                    <IconButton
+                        onClick={() => {
+                                this.handleButtonClick(key)
+                            }
+                        }
+                        variant="raised"
+                        color="primary">
+                        <Icon className="material-icons">{iconType}</Icon>
+                    </IconButton>
+                    
+                    <div style = {shouldDisplayStyle}>
+                        <h6>{key}</h6>
+                        <p>{str}</p>
+                    </div>
                 </div>
             );
         } else {
-            return (
-                <div >
-                    <p> {key + ": " + value} </p>
-                </div>        
-            );
+            if((typeof value !== 'object' && firstPass) || (typeof value === 'object' && !firstPass))
+                return (
+                    <div >
+                        <p> {key + ": " + value} </p>
+                    </div>        
+                );
         }
     }
 
@@ -96,8 +190,15 @@ class CursorPopup extends Component {
                     <div>
                         <h3>Cursor Properties</h3>
                         {
+                            //Renders all non objects first
                             Object.keys(this.state.obj).map(key => {
-                                return this.helper(key, this.state.obj[key]);
+                                return this.helper(key, this.state.obj[key], true);
+                            })
+                        }
+                        {
+                            //Renders objects in a second sweep
+                            Object.keys(this.state.obj).map(key => {
+                                return this.helper(key, this.state.obj[key], false);
                             })
                         }
                     </div>
