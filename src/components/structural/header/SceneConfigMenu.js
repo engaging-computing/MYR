695a8641aa4c343cf0d8df803746ee7ae328684c
Keith Valin <keith@mailtux.net>
2019-09-09 14:36:00 -0400

    Narrowed down settings that would trigger exit confirmation, instead of asking for it if the user accessed settings

--- src/components/structural/header/SceneConfigMenu.js
+++ src/components/structural/header/SceneConfigMenu.js
@@ -81,8 +81,8 @@ class ConfigModal extends Component {
             open: false,
             skyColor: this.props.scene.settings.color,
             displaySkyColorPicker: false,
-            displayFloorColorPicker: false,
             anchorEl: null,
+            displayFloorColorPicker: false,
             qrCodeOpen: false,
             pwProtectOpen: false,
             shareOpen: false,
@@ -90,13 +90,16 @@ class ConfigModal extends Component {
             email: "",
             sendTo: [],
             collectionID: "",
-            value: "a"
+            value: "a",
+            settingsChanged: false
         };
         this.emailRef = React.createRef();
 
         window.addEventListener("beforeunload", (event) => {
-            event.preventDefault();
-            event.returnValue = "You may have unsaved changes!";
+            if(this.state.settingsChanged){
+                event.preventDefault();
+                event.returnValue = "You may have unsaved changes!";
+            }
         });
     }
 
@@ -239,6 +242,7 @@ class ConfigModal extends Component {
             <ButtonBase
                 style={style}
                 onClick={() => {
+                    this.setState({ settingsChanged: true });
                     return this.props.sceneActions.changeView();
                 }
                 }
@@ -260,7 +264,10 @@ class ConfigModal extends Component {
         return (
             <ButtonBase
                 style={style}
-                onClick={() => this.props.sceneActions.toggleFly()} >
+                onClick={() => {
+                    this.props.sceneActions.toggleFly();
+                    this.setState({ settingsChanged: true });
+                }} >
                 {
                     this.props.scene.settings.canFly
                         ? <Icon className="material-icons">toggle_on</Icon>
@@ -281,6 +288,7 @@ class ConfigModal extends Component {
                 onClick={() => {
                     this.props.handleRender();
                     this.props.sceneActions.toggleCoordSky();
+                    this.setState({ settingsChanged: true });
                 }} >
                 {
                     this.props.scene.settings.showCoordHelper
@@ -302,6 +310,7 @@ class ConfigModal extends Component {
                 onClick={() => {
                     this.props.handleRender();
                     this.props.sceneActions.toggleFloor();
+                    this.setState({ settingsChanged: true });
                 }} >
                 {
                     this.props.scene.settings.showFloor
@@ -384,6 +393,7 @@ class ConfigModal extends Component {
                 onClick={() => {
                     this.props.handleRender();
                     this.handleSkyColorClick();
+                    this.setState({ settingsChanged: true });
                 }}>
                 <Icon className="material-icons">color_lens</Icon>
                 Edit Sky Color
@@ -398,6 +408,7 @@ class ConfigModal extends Component {
                 onClick={() => {
                     this.props.handleRender();
                     this.handleFloorColorClick();
+                    this.setState({ settingsChanged: true });
                 }}>
                 <Icon className="material-icons">color_lens</Icon>
                 Edit Floor Color
