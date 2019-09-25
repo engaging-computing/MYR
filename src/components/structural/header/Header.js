2c18cf109cdc74554c833e2e82423d93db20ee19
Keith Valin <keith@mailtux.net>
2019-09-25 11:34:54 -0400

    Scenes without a name now get named 'Untitled Scene'

--- src/components/structural/header/Header.js
+++ src/components/structural/header/Header.js
@@ -269,6 +269,7 @@ class Header extends Component {
                 <TextField id="name-helper"
                     value={sceneName ? sceneName : ""}
                     label="Scene Name"
+                    placeholder={sceneName ? sceneName : "Untitled Scene"}
                     onBlur={this.handleNameChange}
                     onChange={this.handleNameChange} />
                 <TextField
@@ -349,7 +350,7 @@ class Header extends Component {
             imgRef.putString(img, "data_url").then(() => {
                 // Put the new document into the scenes collection
                 scenes.doc(projectId).set({
-                    name: this.props.scene.name,
+                    name: (this.props.scene.name ? this.props.scene.name : "Untitled Scene"),
                     desc: this.props.scene.desc,
                     code: text,
                     uid: this.props.user.uid,
