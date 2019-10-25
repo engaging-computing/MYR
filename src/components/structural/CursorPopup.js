8dce2c819c4e26bef6e2a00c9f5ddf1a1556348a
John Swanson <john@swansonwriting.com>
2019-10-25 12:17:46 -0400

    reformatted the param input form, minor refactoring

--- src/components/structural/CursorPopup.js
+++ src/components/structural/CursorPopup.js
@@ -539,8 +539,8 @@ class CursorPopup extends Component {
         } : null;
       }
 
-    colorNameToHex(colour){
-        const colours = {"aliceblue":"#f0f8ff","antiquewhite":"#faebd7","aqua":"#00ffff","aquamarine":"#7fffd4","azure":"#f0ffff",
+    colorNameToHex(color){
+        const colors = {"aliceblue":"#f0f8ff","antiquewhite":"#faebd7","aqua":"#00ffff","aquamarine":"#7fffd4","azure":"#f0ffff",
         "beige":"#f5f5dc","bisque":"#ffe4c4","black":"#000000","blanchedalmond":"#ffebcd","blue":"#0000ff","blueviolet":"#8a2be2","brown":"#a52a2a","burlywood":"#deb887",
         "cadetblue":"#5f9ea0","chartreuse":"#7fff00","chocolate":"#d2691e","coral":"#ff7f50","cornflowerblue":"#6495ed","cornsilk":"#fff8dc","crimson":"#dc143c","cyan":"#00ffff",
         "darkblue":"#00008b","darkcyan":"#008b8b","darkgoldenrod":"#b8860b","darkgray":"#a9a9a9","darkgreen":"#006400","darkkhaki":"#bdb76b","darkmagenta":"#8b008b","darkolivegreen":"#556b2f",
@@ -554,18 +554,15 @@ class CursorPopup extends Component {
         "lightyellow":"#ffffe0","lime":"#00ff00","limegreen":"#32cd32","linen":"#faf0e6",
         "magenta":"#ff00ff","maroon":"#800000","mediumaquamarine":"#66cdaa","mediumblue":"#0000cd","mediumorchid":"#ba55d3","mediumpurple":"#9370d8","mediumseagreen":"#3cb371","mediumslateblue":"#7b68ee",
         "mediumspringgreen":"#00fa9a","mediumturquoise":"#48d1cc","mediumvioletred":"#c71585","midnightblue":"#191970","mintcream":"#f5fffa","mistyrose":"#ffe4e1","moccasin":"#ffe4b5",
-        "navajowhite":"#ffdead","navy":"#000080",
-        "oldlace":"#fdf5e6","olive":"#808000","olivedrab":"#6b8e23","orange":"#ffa500","orangered":"#ff4500","orchid":"#da70d6",
+        "navajowhite":"#ffdead","navy":"#000080","oldlace":"#fdf5e6","olive":"#808000","olivedrab":"#6b8e23","orange":"#ffa500","orangered":"#ff4500","orchid":"#da70d6",
         "palegoldenrod":"#eee8aa","palegreen":"#98fb98","paleturquoise":"#afeeee","palevioletred":"#d87093","papayawhip":"#ffefd5","peachpuff":"#ffdab9","peru":"#cd853f","pink":"#ffc0cb","plum":"#dda0dd","powderblue":"#b0e0e6","purple":"#800080",
         "rebeccapurple":"#663399","red":"#ff0000","rosybrown":"#bc8f8f","royalblue":"#4169e1",
         "saddlebrown":"#8b4513","salmon":"#fa8072","sandybrown":"#f4a460","seagreen":"#2e8b57","seashell":"#fff5ee","sienna":"#a0522d","silver":"#c0c0c0","skyblue":"#87ceeb","slateblue":"#6a5acd","slategray":"#708090","snow":"#fffafa","springgreen":"#00ff7f","steelblue":"#4682b4",
         "tan":"#d2b48c","teal":"#008080","thistle":"#d8bfd8","tomato":"#ff6347","turquoise":"#40e0d0",
-        "violet":"#ee82ee",
-        "wheat":"#f5deb3","white":"#ffffff","whitesmoke":"#f5f5f5",
-        "yellow":"#ffff00","yellowgreen":"#9acd32"};
+        "violet":"#ee82ee","wheat":"#f5deb3","white":"#ffffff","whitesmoke":"#f5f5f5","yellow":"#ffff00","yellowgreen":"#9acd32"};
 
-        if (typeof colours[colour.toLowerCase()] != 'undefined')
-            return colours[colour.toLowerCase()];
+        if (typeof colors[color.toLowerCase()] != 'undefined')
+            return colors[color.toLowerCase()];
 
         return false;
     }
@@ -576,12 +573,10 @@ class CursorPopup extends Component {
 
         if(shouldRenderObjects) {
             //This maps out all of the key/value pairs in an object
-            let str = Object.keys(value).map(k => {
+            const str = Object.keys(value).map(k => {
                 return this.helper(k, value[k], (Boolean(typeof value[k] !== "object")));
-            })
-
+            });
             const iconType = this.getIconType(key);
-
             const shouldDisplayStyle = { "display": this.getOpen(key) }
 
             return (
@@ -655,14 +650,22 @@ class CursorPopup extends Component {
 
     paramInputHelper = () => {
         return (
-            <>
-            {
-                this.state.params.map(param => (
-                    <input id = {param} class = "param_input" type = "text" placeholder = {param}/>
-                ))
-            }
-                <input type = "submit" onClick = {this.handleRenderParams}/>
-            </>
+            <div className = "row" id = "input_row">
+                <div className = "col-6">
+                    {
+                        this.state.params.map(param => (
+                            <input id = {param} class = "param_input" 
+                                type = "text" placeholder = {param}/>
+                        ))
+                    }
+                </div>
+                <div className = "col-3">
+                    <input id = "submit_button" type = "submit" onClick = {this.handleRenderParams}/>
+                </div>
+                <div className = "col-12">
+                    <Divider id = "param_divider" variant="middle" />
+                </div>
+            </div>
         );
     }
 
