3e263a1e0ed6beda20ed15665a29e3dea4ef3aa5
Keith Valin <keith@mailtux.net>
2020-03-02 10:58:20 -0500

    Dismiss button is to the right of text, which is now center aligned

--- src/components/structural/header/Banner.js
+++ src/components/structural/header/Banner.js
@@ -10,14 +10,24 @@ class Banner extends Component {
     }
     render = () => {
         const style = {
-            backgroundColor: this.props.color,
-            width: "100%"
+            div:{
+                backgroundColor: this.props.color,
+                width: "100%",
+                textAlign: "center"
+            },
+            p: {
+                display: "inline"
+            },
+            button: {
+                float: "right",
+                padddingBottom: "2px"
+            }
         };
 
         return (this.state.isOpen ? 
-            <div style={style}>
-                {this.props.message}
-                <Button onClick={() => {
+            <div style={style.div}>
+                <p style={style.p}>{this.props.message}</p>
+                <Button style={style.button} onClick={() => {
                     this.setState({isOpen: false});
                 }}>Dismiss</Button>
             </div>
