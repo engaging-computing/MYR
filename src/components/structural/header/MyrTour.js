7f3cef9116659ca774de13d7585b826c8ed28da1
Samuel Zuk <Samuel_Zuk@student.uml.edu>
2020-05-24 14:51:35 -0400

    Moved tour steps to separate file in MYR folder

--- src/components/structural/header/MyrTour.js
+++ src/components/structural/header/MyrTour.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Tour from "reactour";
 import { Button } from "@material-ui/core";
 import * as layoutTypes from "../../../constants/LayoutTypes";
+import { TourSteps } from "../../../myr/tour";
 
 class MyrTour extends Component {
     constructor(props) {
@@ -25,7 +26,7 @@ class MyrTour extends Component {
                 {!isDisabled ?
                     <React.Fragment>
                         <Tour
-                            steps={formatSteps(steps)}
+                            steps={formatSteps(TourSteps)}
                             maskClassName="mask"
                             isOpen={this.props.tourOpen}
                             onAfterOpen={() => {
@@ -78,63 +79,5 @@ const formatSteps = (steps) => {
         return step;
     });
 };
-
-const steps = [
-    {
-        selector: "#ace-editor",
-        content: "This is the editor. You can create 3D scenes using JavaScript " +
-            "and a special set of instructions or functions to MYR.\nThe editor can be " +
-            "toggled on and off by opening the settings menu in the top right and " +
-            "clicking the \"View Editor\" switch."
-    },
-    {
-        selector: "#play-btn",
-        content: "The Play button will render the scene."
-    },
-    {
-        selector: "#stop-btn",
-        content: "The Stop button will stop the scene. \nUse this to save battery."
-    },
-    {
-        selector: "#scene",
-        content: "The View is where you can see your work. " +
-            "You can navigate the scene with the following controls: \n" +
-            "W: Forward \n" +
-            "A: Left\n" +
-            "S: Backward\n" +
-            "D: Right\n" +
-            "Space: Fly Upwards\n" +
-            "Shift: Fly Downwards\n" +
-            "To view the scene in VR, click the goggles!"
-    },
-    {
-        selector: "#new-btn",
-        content: "Create a new scene from scratch. Be sure to save first!",
-    },
-    {
-        selector: "#save-btn",
-        content: "Save your work.",
-    },
-    {
-        selector: "#open-btn",
-        content: "See previous work and view examples.",
-    },
-    {
-        selector: "#ref-btn",
-        content: "Use the Reference to see all MYR has to offer.",
-    },
-    {
-        selector: "#configure-scene",
-        content: "Modify and share your scene including setting the background color, enabling the grid, and enabling flying.",
-    },
-    {
-        selector: "#select-course",
-        content: "View the list of available courses to get you started on using MYR.",
-    },
-    {
-        selector: "#user",
-        content: "You can log in with a Google account in order to save your scenes.",
-    },
-];
 
 export default MyrTour;
