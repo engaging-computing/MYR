57a2a78934eea01483ba35830f9d82f810d9fa86
Patrick Sullivan <psullivang7@gmail.com>
2021-04-07 12:51:19 -0400

    Updated model URLs to work with CORS

--- src/components/structural/Models.js
+++ src/components/structural/Models.js
@@ -1,8 +1,8 @@
 let ModelPack = new Map();
 // Models that have been cleared for us to host
-ModelPack.set("sword", "https://github.com/beewyka819/MYR/raw/models/assets/gltf/sword_2.9.glb");
-ModelPack.set("test", "https://github.com/beewyka819/MYR/raw/models/assets/gltf/test.glb");
-ModelPack.set("duck", "https://github.com/KhronosGroup/glTF-Sample-Models/raw/master/2.0/Duck/glTF-Binary/Duck.glb");
+ModelPack.set("sword", "https://raw.githubusercontent.com/beewyka819/MYR/models/assets/gltf/sword_2.9.glb");
+ModelPack.set("test", "https://raw.githubusercontent.com/beewyka819/MYR/models/assets/gltf/test.glb");
+ModelPack.set("duck", "https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/Duck/glTF-Binary/Duck.glb");
 
 // Below are models for which licenses need to be reviewed before hosting
 // N/A
